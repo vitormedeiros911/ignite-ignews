09e13f60d040e5138fb516bba5ab871e9e60c697
vitormedeiros911 <vitormedeiros911@outlook.com>
2021-06-05 22:21:41 -0300

    Adiciona componentes na página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello world</h1>;
+	return (
+		<>
+			<Head>
+				<title>Home | ig.news</title>
+			</Head>
+
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span>👏 Hey, welcome</span>
+					<h1>
+						News about <br /> the <span>React</span> world
+					</h1>
+					<p>
+						Get access to all the publications <br />
+						<span>for $9.90 month</span>
+						<SubscribeButton />
+					</p>
+				</section>
+
+				<img src="/images/avatar.svg" alt="Girl coding" />
+			</main>
+		</>
+	);
 }
